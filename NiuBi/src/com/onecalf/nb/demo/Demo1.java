bb3ac76aa788d6ef45aac8d5149f9c7aa1c88e87
jiulu <zhanghongjun228@163.com>
2018-09-05 10:22:07 +0800

    demo

--- NiuBi/src/com/onecalf/nb/demo/Demo1.java
+++ NiuBi/src/com/onecalf/nb/demo/Demo1.java
@@ -3,7 +3,8 @@ package com.onecalf.nb.demo;
 public class Demo1 {
     public static void main(String[] args){
 
-
+        String str = System.getProperty("user.home");
+        System.out.println(str);
 
     }
 
