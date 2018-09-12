e5ae7d162d13e320d0c0a6054d4bd390e83a5027
张红军 <zhanghongjun228@163.com>
2018-09-12 09:08:21 +0800

    监听activity生命周期

---
+++ Hard/app/src/main/java/com/onecalf/hard/HardApplication.java
@@ -0,0 +1,51 @@
+package com.onecalf.hard;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+public class HardApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //统一的监听activity的生命周期
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+}
