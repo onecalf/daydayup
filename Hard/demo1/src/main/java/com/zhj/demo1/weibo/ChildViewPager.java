7bd0e8bc733375e9db6a33f9396633b030e31277
张红军 <zhanghongjun228@163.com>
2018-12-08 14:23:29 +0800

    添加viewpager

--- Hard/demo1/src/main/java/com/zhj/demo1/weibo/ChildViewPager.java
+++ Hard/demo1/src/main/java/com/zhj/demo1/weibo/ChildViewPager.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 public class ChildViewPager extends ViewPager {
     public ChildViewPager(@NonNull Context context) {
@@ -13,6 +14,11 @@ public class ChildViewPager extends ViewPager {
 
     public ChildViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
     }
 }
 
